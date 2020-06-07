6892190a39e63db37dce197fdc0fba40ec4513f2
Arthur Tavares Quintao <arthurtquintao@gmail.com>
2020-06-07 15:43:39 -0400

    Create users table

---
+++ finance/finance.sql
@@ -0,0 +1,2 @@
+-- Query used to create the table
+CREATE TABLE 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' TEXT NOT NULL, 'hash' TEXT NOT NULL, 'cash' NUMERIC NOT NULL DEFAULT 10000.00 );
